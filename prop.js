79f7a3f947f88a9b772f195c0800aef497077198
shiftalowpv <bitchunk@kemono.jp>
2014-08-24 13:37:23 +0900

    ファイル追加

---
+++ prop.js
@@ -0,0 +1,17 @@
+/**
+ * Properties
+ * Since 2013-11-19 07:43:37
+ * @author しふたろう
+ */
+
+//canvas
+var VIEWMULTI = 2;//基本サイズ拡大倍数
+var CHIPCELL_SIZE = 8;//基本サイズ1辺り 8*8
+var DISPLAY_WIDTH = 320;//表示基本幅
+var DISPLAY_HEIGHT = 240;//表示基本高
+
+var IMAGE_DIR = './img/';
+
+//keyevent
+var KEYCONTROLL_HOLDTIME = 16; //キー固定判定時間[fps]
+
