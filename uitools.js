7eaee6b7ea78b39bb9847ce2cfa82d947f6213ba
oshiimizunohuta <g.ebodai@gmail.com>
2017-02-05 18:25:33 +0900

    sceneOrder： ┗パラメータ初期生成の共通化 ┗isLoopedの判定修正 ┗総継続時間の算出 ┗現在のシーンを削除関数追加 ┗デバッグ表示を強化 ┗カラー定数を追加

--- uitools.js
+++ uitools.js
@@ -24,6 +24,7 @@ CUCursor.prototype = {
 		this.pos_pre = {x: 0, y:0, z: 0};
 		this.cells = {x: 0, y: 0, z: 0};
 		this.looped = {x: 0, y: 0, z: 0};
+		this.outside = {x: 0, y: 0, z: 0};
 		this.disablePos = [];
 		this.cellReturn = true;
 		this.name = 'cursor';
@@ -133,6 +134,25 @@ CUCursor.prototype = {
 		}
 		return a.join(' ');
 	},
+	
+	//他のカーソル切替時、isLoopedと組み合わせる
+	out: function()
+	{
+//		this.pos[axis] = this.cells[axis];
+		this.outside = {x: this.looped.x, y: this.looped.y, z: this.looped.z}
+		return this.outside;
+	},
+	in: function(outside)
+	{
+		var cells = this.cells, a, pos = this.pos;
+		for(a in pos){
+			if(outside[a] > 0){
+				pos[a] = 0;
+			}else if(outside[a] < 0){
+				pos[a] = cells[a] - 1;
+			}
+		}
+	},
 
 	move: function(x, y, z)
 	{
@@ -146,10 +166,10 @@ CUCursor.prototype = {
 			pre[a] = pos[a];
 			pos[a] += nums[a];
 			if(pos[a] >= cells[a]){
-				loop[a]++;
+				loop[a] = pos[a] - cells[a] + 1;
 				pos[a] = pos[a] % cells[a];
 			}else if(pos[a] < 0){
-				loop[a]--;
+				loop[a] = pos[a];
 				//-pos
 				pos[a] = (cells[a] + pos[a]) % cells[a];
 			}else{
@@ -214,10 +234,10 @@ CUCursor.prototype = {
 	
 	isLooped: function(axis)
 	{
-		if(axis != null){
+		if(axis == null){
 			return this.looped;
 		}
-		if(this.looped[axis] != false){
+		if(this.looped[axis] != null){
 			return this.looped[axis];
 		}
 		return null;
@@ -234,8 +254,35 @@ SceneTransition.prototype = {
 		this.sceneOrder = [];
 		this.scenePrevious = null;
 		 this.sceneCurrent = null;
+		 
+		 this.nullFunc = function(info){
+			 return false;
+		 };
 	},
 	
+	makeParams: function(funcName, duration, params, count, remove){
+		count = count == null ? 0 : count;
+		remove = remove == null ? false : remove;
+		var res = {name: funcName, duration: duration, count: count, remove: remove, params: params};
+		return res;
+	},
+	
+	find: function(name, inIndex){
+		var f;
+//		inIndex = inIndex == null ? 0 : inIndex;
+		if(this.sceneCurrent.name == name){
+			return this.sceneCurrent;
+		}
+		f = this.sceneOrder.find(function(a){
+			return a.name == name;
+		});
+//		if(f == null){
+//			return f;
+//		}
+		
+		return f;
+//		return f[inIndex];
+	},
 
 	current: function(name){
 		var rem, current;
@@ -254,22 +301,50 @@ SceneTransition.prototype = {
 		return this.sceneCurrent;
 	},
 	
+	sumDuration: function()
+	{
+		var s = 0;
+		this.sceneOrder.forEach(function(a){
+			s += a.duration;
+		});
+		if(this.currentOrder != null){
+			s += this.currentOrder.duration - this.currentOrder.count;
+		}
+		return s;
+	},
+	
 //	pushOrder: function(funcName, duration, params){
 	pushOrder: function(funcName, duration, params){
-		this.sceneOrder.push({name: funcName, duration: duration, count: 0, params: params});
+		params = params == null ? {} : params;
+		this.sceneOrder.push(this.makeParams(funcName, duration, params));
 	},
 	
 	pushOrderFunc: function(func, duration, params){
-		this.sceneOrder.push({func: func, duration: duration, count: 0, params: params});
+		params = params == null ? {} : params;
+		this.sceneOrder.push(this.makeParams(funcName, duration, params));
 	},
 	
 	unshiftOrder: function(funcName, duration, params){
 		var current = this.sceneCurrent;
+		params = params == null ? {} : params;
 		if(current != null){
 			this.sceneCurrent = null;
 			this.sceneOrder.unshift({name: current.name, duration: current.duration, count: current.count, params: current.params});
+			this.sceneOrder.unshift(this.makeParams(funcName, duration, params));
 		}
 		this.sceneOrder.unshift({name: funcName, duration: duration, count: 0, params: params});
+	},
+	
+	removeCurrentOrder: function(){
+		if(this.sceneCurrent != null){
+			//即消し
+			this.scenePrevious = this.sceneCurrent;
+			this.sceneCurrent = null;
+		}else if(this.sceneOrder.length > 0){
+			//予約する
+			this.sceneOrder[0].remove = true;
+		}
+
 	},
 	
 	removeOrder: function(name){
@@ -310,7 +385,7 @@ SceneTransition.prototype = {
 		current = current == null ? order.shift() : current;
 		this.sceneCurrent = current;
 		
-		if(caller[current.name](current)){
+		if((current.name != null && caller[current.name](current)) || current.remove){
 			this.scenePrevious = this.sceneCurrent;
 			this.sceneCurrent = null;
 		}
@@ -325,21 +400,52 @@ SceneTransition.prototype = {
 function drawDebugCell(scroll, pointControll, wordprint, color){
 	var bg = SCROLL.sprite
 		, pos = pointControll.getMovePos()
+		, start = pointControll.getStartPos()
+		, right = pointControll.getState('right')
+		, left = pointControll.getState('left')
 		, cto = cellhto, toc = tocellh
 		, x, y, w = cto(1)
+		, r
 		, str
 		, backScroll = wordprint.getScroll()
+		, bgcolor = COLOR_BLACK
 	;
 	
 	color = color == null ? COLOR_WHITE : color;
 	
+	
 	pos = {x: toc(pos.x), y: toc(pos.y)};
-	x = pos.x - (pos.x < 1 ? 0 : 1);
-	y = pos.y - (pos.y < 2 ? -1 : 2);
-	str = (pos.x < 10 ? 'x:0' : 'x:') + pos.x + '$n' + (pos.y < 10 ? 'y:0' : 'y:') + pos.y + '';
+	start = {x: toc(start.x), y: toc(start.y)};
+	if(left){
+		r = makeRect([start.x, start.y, pos.x, pos.y].join(' ') + ' :pos' );
+		scroll.debugRect(makeRect(r.toString + ' *8'), color);
+		
+		str = numFormat(r.x, 2) + ':' + numFormat(r.y, 2) + '$n' 
+			+ numFormat(r.w, 2) + ':' + numFormat(r.h, 2);
+	
+		if(r.w > 2 && r.h > 1){
+			x = start.x >= pos.x ? start.x - 1.5 : start.x;
+			y = start.y >= pos.y ? start.y - 1 : start.y;
+		}else{
+			x = start.x >= pos.x ? r.ex - 2.5 : r.x;
+			y = start.y >= pos.y ? r.ey : r.y - 2;
+		}
+		x = x > 37.5 ? 37.5 : x;
+		y = y > 28 ? 28 : y;
+		x += x < 0 ? -x : 0;
+		y += y < 0 ? -y : 0;
+			
+		if(r.isContain(x, y)){
+//			color = COLOR_BLACK;
+		}
+	}else{
+		str = (pos.x < 10 ? 'x:0' : 'x:') + pos.x + '$n' + (pos.y < 10 ? 'y:0' : 'y:') + pos.y + '';
+		scroll.debugRect(makeRect(cto(pos.x), cto(pos.y), w, w), color);
+		x = pos.x - (pos.x < 1 ? 0 : 1);
+		y = pos.y - (pos.y < 2 ? -1 : 2);
+	}
 	wordprint.setScroll(scroll);
-	wordprint.print(str, cto(x), cto(y));
-	scroll.debugRect(makeRect(cto(pos.x), cto(pos.y), w, w), color);
+	wordprint.print(str, cto(x), cto(y), color, bgcolor);
 	
 	//戻す
 	wordprint.setScroll(backScroll);
@@ -352,4 +458,19 @@ var COLOR_WHITE = [252, 252, 252, 255];
 var COLOR_HLGREEN = [184, 248, 184, 255];
 var COLOR_OCEAN = [0, 64, 88, 255];
 var COLOR_LBLUE = [164, 228, 252, 255];
-var COLOR_RED = [247, 49, 0, 255];
+var COLOR_RED = [248, 56, 0, 255]; //canvasdraw.jsを上書き
+
+var COLOR_MIDNIGHT = [0, 0, 188, 255];
+var COLOR_MIDBLUE = [0, 88, 248, 255];
+var COLOR_ROYALBLUE = [104, 136, 252, 255];
+var COLOR_CORNFLOWER = [184, 184, 248, 255];
+
+var COLOR_FOREST = [0, 120, 0];
+var COLOR_LIMEGREEN = [0, 184, 0, 255];
+//var COLOR_GREENYELLOW = [216, 248, 120, 255];
+var COLOR_GREENYELLOW = [216, 248, 120, 255];
+
+var COLOR_BLUE = [0, 0, 252, 255];
+
+var COLOR_WHERET = [252, 224, 168, 255];
+var COLOR_DARKGOLDENROD = [172, 124, 0, 255];
